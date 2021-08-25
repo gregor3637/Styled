9f103dd154a74f9918c859cc52236415fbffc229
mihailm956 <mihailm956@gmail.com>
2021-08-25 20:15:33 +0300

    About Page

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import ImageSection from "../Components/ImageSection";
+import Title from "../Components/Title";
+import { MainLayout } from "../styles/Layouts";
 
 const AboutPage = (props) => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <MainLayout>
+      <AboutStyled>
+        <Title title={"About Me"} span={"About Me"} />
+        <ImageSection />
+      </AboutStyled>
+    </MainLayout>
+  );
+};
 
+const AboutStyled = styled.section``;
 export default AboutPage;
